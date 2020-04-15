324cb9684cb35922db678bb9bb20bbe5c8ae5012
Paulina Szarpak <szarpak.paulina@gmail.com>
2020-04-15 20:18:54 +0200

    Zad_1_1 + opis

--- Zad_1_1.py
+++ Zad_1_1.py
@@ -1,3 +1,14 @@
+#Napisz program, który prosi użytkownika (przez `input()`), żeby podał, ile kosztuje kilo ziemniaków.
+# Niech program policzy i wyświetli, ile trzeba będzie zapłacić za pięć kilo ziemniaków.
+
+#Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał, ile kosztuje kilo ziemniaków i ile kilo chce kupić.
+# Niech program policzy i wyświetli, ile trzeba będzie zapłacić za te ziemniaki.
+
+#Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał, ile kosztuje kilo ziemniaków,
+# ile kilo ziemniaków chce kupić, ile kosztuje kilo bananów i ile kilo bananów chce kupić.
+# Niech program policzy i wyświetli, ile trzeba będzie zapłacić za te ziemniaki i banany razem.
+# I niech program sprawdzi i powie, za co trzeba będzie zapłacić więcej - za banany czy za ziemniaki.
+
 cena_ziemniakow = float(input("Cena za kg ziemniaków: "))
 kg_ziemniakow = float(input("Napisz proszę, ile kg ziemniaków chcesz kupić: "))
 cena_bananow = float(input("Cena za kg bananów: "))
